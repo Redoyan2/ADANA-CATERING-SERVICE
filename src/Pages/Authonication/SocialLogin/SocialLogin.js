5df53ebc629766f7bb5dc396f437f9db74a55349
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-17 17:27:47 +0600

    google authonication

--- src/Pages/Authonication/SocialLogin/SocialLogin.js
+++ src/Pages/Authonication/SocialLogin/SocialLogin.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../init.firebase';
+import Loading from '../../Shared/Loding/Loading';
 
 const SocialLogin = () => {
-
-
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    let errorCase;
+
+    if(loading){
+        return <Loading></Loading>
+    }
+    if (error){
+        errorCase = <p className='text-danger'>Error: {error?.message} </p>
+    }
 
 
     return (
         <div>
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+            </div>
+            <div className='text-center'>
+            {errorCase}
+            </div>
+            <div className='text-center'>
+                <button className='btn btn-info ms-3'
+                onClick={() => signInWithGoogle()}>
+                    Google SignIn
+                </button>
+                <button className='btn btn-info ms-3'>
+                    Facebook SignIn
+                </button>
+            </div>
             
         </div>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Collapse } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => {
@@ -8,14 +8,19 @@ const Header = () => {
             <>
                 <Navbar bg="info" variant="dark" expand={true}>
                     <Container>
-                        <Navbar.Brand href="home">ADANA CATERING SERVICE</Navbar.Brand>
+                        <Navbar.Brand href="/">ADANA CATERING SERVICE</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    
                         <Nav className="ms-auto">
                             <Nav.Link href="home"  >Home</Nav.Link>
                             <Nav.Link href="services">Services</Nav.Link>
                             <Nav.Link href="about">About</Nav.Link>
                             <Nav.Link href="blog">Blogs</Nav.Link>
                             <Nav.Link href="contact">Contact Us</Nav.Link>
-                            
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="login"  >Login</Nav.Link>
                         </Nav>
                     </Container>
                 </Navbar>
