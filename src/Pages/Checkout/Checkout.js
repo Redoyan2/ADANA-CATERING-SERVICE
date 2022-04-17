f69fb54b439bd29fbcd7fe0129ac095d1a2e4354
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-18 05:04:41 +0600

    checkout page

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Checkout = () => {
     return (
-        <div>
-            This is Checkout pages
+        <div className='m-5'>
+            <Form className='w-50'>
+            <Form.Label>Name:</Form.Label>
+                    <Form.Control type="text" placeholder="Your Name" />
+                    
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Address:</Form.Label>
+                    <Form.Control type="password" placeholder='Your address' />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accept Terms and Condition" />
+                </Form.Group>
+                <Button variant="info" type="submit">
+                    Place Order
+                </Button>
+            </Form>
         </div>
     );
 };
